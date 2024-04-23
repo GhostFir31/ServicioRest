673f558503a3626a43feb047233717a399382516
GhostFir31 <j3sus0marle@gmail.com>
2024-04-23 15:26:01 -0700

    fix

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port "+ PORT);
 });
